1c7ce2963c8f506b66701b966ecc4a90b0429812
Archit Chandra <architchandra@gmail.com>
2022-03-16 15:26:00 +0530

    Set up container and background color

--- src/assets/styles/tailwind.config.js
+++ src/assets/styles/tailwind.config.js
@@ -1,8 +1,25 @@
+const colors = require('tailwindcss/colors');
+
+
+
 module.exports = {
   content: [
     './src/components/**/*.js',
+    './public/index.html',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      grey: colors.zinc[100],
+    },
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        sm: '1280px',
+      },
+    },
     extend: {},
   },
   plugins: [],
--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,17 @@ import TextInput from './TextInput';
 
 function App() {
   return (
-    <>
-      <h1>
-        Word Limit
-      </h1>
-      <h3>
-        A word and character counter built using React and Tailwind.
-      </h3>
-      <TextInput />
-    </>
+    <main className="my-12">
+      <div className="container text-center">
+        <h1>
+          Word Limit
+        </h1>
+        <h3>
+          A word and character counter built using React and Tailwind.
+        </h3>
+        <TextInput />
+      </div>
+    </main>
   );
 }
 
