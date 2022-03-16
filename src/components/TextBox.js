abe2693bd6e3c9031a1ed531329379f907aa68d0
Archit Chandra <architchandra@gmail.com>
2022-03-16 16:54:10 +0530

    Add padding to textbox

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,7 +1,7 @@
 function TextBox({ text, handleOnChange }) {
   return (
     <textarea
-      className="w-full mb-8 rounded-md border border-dark-grey"
+      className="w-full mb-8 p-2 rounded-md border border-dark-grey"
       rows="12"
       value={text}
       onChange={handleOnChange}
