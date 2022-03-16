81050fc9ab070a31fa95a149537cc104c4bd7edc
Archit Chandra <architchandra@gmail.com>
2022-03-16 15:38:18 +0530

    Improve app title area styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,14 @@ function App() {
   return (
     <main className="my-12">
       <div className="container text-center">
-        <h1>
-          Word Limit
-        </h1>
-        <h3>
-          A word and character counter built using React and Tailwind.
-        </h3>
+        <div className="mb-6">
+          <h1 className="title-h1">
+            Word Limit
+          </h1>
+          <h3>
+            A word and character counter built using React and Tailwind.
+          </h3>
+        </div>
         <TextInput />
       </div>
     </main>
