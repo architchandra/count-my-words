fba09874842205723dfd042ebcda2df1203ae2ac
Archit Chandra <architchandra@gmail.com>
2022-03-16 16:05:00 +0530

    Set up text change logic

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+
+
+
 function TextBox() {
+  const [text, setText] = useState('');
+  
   return (
-    <textarea className="w-full" rows="12"></textarea>
+    <textarea
+      className="w-full"
+      rows="12"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    ></textarea>
   );
 }
 
