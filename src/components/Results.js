cc0303e8567caaa947f308990debc7b11e96e05f
Archit Chandra <architchandra@gmail.com>
2022-03-26 22:58:57 +0530

    Improve styling of results area, refactor Results code to be more DRY

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,15 +5,25 @@ function Results({ text }) {
   function getWordCount(text) {
     return text.split(/\W/).filter(word => !!word).length;
   }
+
+  const resultsList = [{
+    label: 'characters',
+    count: getCharCount(text),
+  },{
+    label: 'words',
+    count: getWordCount(text),
+  }];
   
   return (
-    <div class="flex mb-6">
-      <p class="mr-4 last:mr-0">
-        {getCharCount(text)} characters
-      </p>
-      <p class="mr-4 last:mr-0">
-        {getWordCount(text)} words
-      </p>
+    <div class="flex mb-6 p-4 rounded-md border border-dark-grey">
+      {resultsList.map((result, index) => 
+        <p class="mr-4 pr-4 last:mr-0 last:pr-0 border-r last:border-none border-dark-grey text-sm" key={index}>
+          <span class="block text-2xl">
+            {result.count}
+          </span>
+          {result.label}
+        </p>
+      )}
     </div>
   );
 }
