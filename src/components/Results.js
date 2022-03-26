2421c5da5061bfdc137fd556acffa897f1e21ac3
Archit Chandra <architchandra@gmail.com>
2022-03-26 23:01:08 +0530

    Increase font size of result counts

--- src/components/Results.js
+++ src/components/Results.js
@@ -18,7 +18,7 @@ function Results({ text }) {
     <div class="flex mb-6 p-4 rounded-md border border-dark-grey">
       {resultsList.map((result, index) => 
         <p class="mr-4 pr-4 last:mr-0 last:pr-0 border-r last:border-none border-dark-grey text-sm" key={index}>
-          <span class="block text-2xl">
+          <span class="block text-3xl">
             {result.count}
           </span>
           {result.label}
