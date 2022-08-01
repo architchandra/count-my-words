533827b86ec221ebce188471047ca7342d31da5c
Archit Chandra <architchandra@gmail.com>
2022-08-01 16:46:43 +0530

    Move app header to a separate component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="mb-12">
+      <h1 className="mb-1 title-h1 text-orange">
+        Word Limit
+      </h1>
+      <h3>
+        A simple word and character counter. Free to use.
+      </h3>
+    </div>
+  );
+}
+
+
+
+export default Header;
