f07c328a23a907d619258efbb4fe8012737ddad4
Archit Chandra <architchandra@gmail.com>
2022-08-11 17:35:17 +0530

    Add home page link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ import Logo from '../assets/images/logo.svg';
 function Header() {
   return (
     <div className="container mb-12">
-      <div className="flex justify-center items-end mb-2">
+      <Link className="flex justify-center items-end mb-2" href="/">
         <img className="w-12 h-12 mr-2" src={Logo} alt="Count My Words logo" />
         <h1 className="title-h1 text-orange">
           Count My Words
         </h1>
-      </div>
+      </Link>
       <h3 className="title-h3 mb-4">
         A simple word and character counter. Free to use.
       </h3>
