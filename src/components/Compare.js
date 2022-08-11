736da2853fce0762af4188d9b1d8ae5f1e64a198
Archit Chandra <architchandra@gmail.com>
2022-08-11 17:39:54 +0530

    Remove unecessary headings from Compare component

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -4,14 +4,9 @@ import Counter from './Counter';
 
 function Compare() {
   return (
-    <div>
-      <h2 className="title-h2">Compare</h2>
-      <h3 className="title-h3">Compare multiple pieces of text side by side</h3>
-      <div className="md:grid grid-cols-2 gap-12">
-        <Counter />
-        <Counter />
-      </div>
-      
+    <div className="md:grid grid-cols-2 gap-12">
+      <Counter />
+      <Counter />
     </div>
   );
 }
