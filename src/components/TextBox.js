15ca17a8f93a4cd1b3055a33e9b2bdd70329be3c
Archit Chandra <architchandra@gmail.com>
2022-08-12 15:02:40 +0530

    Reduce number of rows in textbox

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -2,7 +2,7 @@ function TextBox({ text, handleOnChange }) {
   return (
     <textarea
       className="w-full p-3 rounded-md border border-dark-grey bg-light-grey placeholder:text-darker-grey"
-      rows="16"
+      rows="12"
       value={text}
       onChange={handleOnChange}
       placeholder="Type or paste your text here. We don't save or share any data with anyone."
